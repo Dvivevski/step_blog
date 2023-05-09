8b1a65c896656cdaf39d5a33d5cf4ba44fe648ef
Dvivevski <vitalii.danylevskyi@gmail.com>
2023-05-09 22:05:02 +0300

    reg_fix+obj(timeformat)

--- frontend/src/helper/objects.js
+++ frontend/src/helper/objects.js
@@ -51,10 +51,8 @@ export const formatDate = (dte) => {
   let minutes = dt.getMinutes();
   let hours = dt.getHours();
 
-  var ampm = hours >= 12 ? "pm" : "am";
-  hours = hours % 12;
-  hours = hours ? hours : 12; // the hour '0' should be '12'
+  
   minutes = minutes < 10 ? "0" + minutes : minutes;
 
-  return `${date}, ${hours}:${minutes} ${ampm.toUpperCase()}`;
+  return `${date}, ${hours}:${minutes}`;
 };
